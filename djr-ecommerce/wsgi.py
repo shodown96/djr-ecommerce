68659f91207925a40cd850b05984f8a9d9466d72
Elijah Soladoye <shodown96@gmail.com>
2020-10-03 23:03:17 +0100

    second deploy

--- djr-ecommerce/wsgi.py
+++ djr-ecommerce/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djr-ecommerce.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djr-ecommerce.settings.prod")
 
 application = get_wsgi_application()
