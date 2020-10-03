1f56cd76a1b888e63d44d7411afbec3d91025f55
Elijah Soladoye <shodown96@gmail.com>
2020-10-03 22:39:27 +0100

    second commit

--- djr-ecommerce/settings/base.py
+++ djr-ecommerce/settings/base.py
@@ -98,7 +98,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
 ACCOUNT_EMAIL_REQUIRED = False
